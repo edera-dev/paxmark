23b6722e2fe8ed92f973a887f19714c5936b13cd
Ariadne Conill <ariadne@ariadne.space>
2024-07-16 15:48:06 -0700

    implement command parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,65 @@
+use std::path::PathBuf;
+use std::env;
+
+use getopts::Options;
+
+fn print_usage(program: &str, opts: Options) {
+    let brief = format!("Usage: {} BINARY [options]", program);
+    print!("{}", opts.usage(&brief));
+}
+
+#[derive(Clone)]
+struct Flag<'a> {
+    disable: char,
+    enable: char,
+    help: &'a str,
+}
+
+const FLAGTABLE: [Flag; 5] = [
+    Flag {disable: 'p', enable: 'P', help: "PAGEEXEC"},
+    Flag {disable: 'e', enable: 'E', help: "EMUTRAMP"},
+    Flag {disable: 'm', enable: 'M', help: "MPROTECT"},
+    Flag {disable: 'r', enable: 'R', help: "RANDMMAP"},
+    Flag {disable: 's', enable: 'S', help: "SEGMEXEC"},
+];
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let program = args[0].clone();
+    let mut flags: String = "".to_string();
+
+    let mut opts = Options::new();
+
+    for flag in FLAGTABLE.iter() {
+        opts.optflag(&flag.disable.to_string(), "", format!("disable {}", flag.help).as_str());
+        opts.optflag(&flag.enable.to_string(), "", format!("enable {}", flag.help).as_str());
+    }
+
+    opts.optflag("h", "help", "display help message");
+
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => { m }
+        Err(f) => { panic!("{}", f.to_string()) }
+    };
+
+    if matches.opt_present("h") {
+        print_usage(&program, opts);
+        return;
+    }
+
+    for flag in FLAGTABLE.iter() {
+        if matches.opt_present(flag.disable.to_string().as_str()) {
+            flags.push_str(flag.disable.to_string().as_str());
+            continue;
+        }
+
+        flags.push_str(flag.enable.to_string().as_str());
+    }
+
+    if matches.free.is_empty() {
+        print_usage(&program, opts);
+        return;
+    }
+
+    let target_binary = PathBuf::from(matches.free[0].clone());
 }
